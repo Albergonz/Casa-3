ff7f9ec9f988ead252f727082e2105c89073dbaf
Acer <Acer@LAPTOP-9SISV8A7>
2024-01-23 18:06:25 +0100

    Aggiungere i file di progetto.

---
+++ Casa 3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casa_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rettangolo rettangolo = new Rettangolo();
+            rettangolo.Base = int.Parse(Console.ReadLine());
+            rettangolo.Altezza = int.Parse(Console.ReadLine());
+            Console.WriteLine("A: {0}", rettangolo.Area());
+            Console.WriteLine("2P: {0}", rettangolo.Perimetro());
+
+            Rettangolo quadrato = new Rettangolo();
+            rettangolo.Base = int.Parse(Console.ReadLine());
+            rettangolo.Altezza = rettangolo.Base;
+            Console.WriteLine("A: {0}", rettangolo.Area());
+            Console.WriteLine("2P: {0}", rettangolo.Perimetro());
+        }
+    }
+}
---
+++ Casa 3/Rettangolo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casa_3
+{
+    internal class Rettangolo
+    { 
+        public int Altezza { get; set; }
+        public int Base{ get; set;}
+
+        public float Area()
+        {
+            return Altezza * Base;
+        }
+
+        public float Perimetro() {
+
+            return (Base * Altezza * 2);
+
+        }
+    }
+}
