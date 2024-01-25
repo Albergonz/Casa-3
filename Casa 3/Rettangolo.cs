2cd0734e2f1850574ec2cf5edf7d1a714ec14a71
Acer <Acer@LAPTOP-9SISV8A7>
2024-01-25 17:06:30 +0100

    mancava un }

--- Casa 3/Rettangolo.cs
+++ Casa 3/Rettangolo.cs
@@ -7,16 +7,17 @@ using System.Threading.Tasks;
 namespace Casa_3
 {
     internal class Rettangolo
-    { 
+    {
         public int Altezza { get; set; }
-        public int Base{ get; set;}
+        public int Base { get; set; }
 
         public float Area()
         {
             return Altezza * Base;
         }
 
-        public float Perimetro() {
+        public float Perimetro()
+        {
 
             return (Base * Altezza * 2);
 
@@ -24,6 +25,7 @@ namespace Casa_3
 
         public override string ToString()
         {
-            return string.Format("Rettangolo di altezza {0} e base{1}",  Altezza, Base);
+            return string.Format("Rettangolo di altezza {0} e base{1}", Altezza, Base);
         }
+    }
 }
